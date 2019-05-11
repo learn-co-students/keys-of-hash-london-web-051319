63070ba0dc928d1a903383c5984544f94744fb23
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-11 19:29:05 +0100

    instance method for hash class - keys of hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # define new array to store result
+    answer_array = []
+    # iterate over the self/object looking at each key and value
+    self.each do |key, value|
+      # if one of the arguments passed includes the value being reviewed, go into code  
+      if arguments.include?(value)
+        # 'shovel'/add the key of the matching value to the array
+        answer_array << key
+      end
+    end
+    answer_array
   end
 end
